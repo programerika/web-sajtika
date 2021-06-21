7fa79a8854f81a54207ea7028a0e631e98ceb272
MilanUzelac1995 <milan.uzelac@programerika.com>
2021-06-21 12:49:54 +0200

    added Navbar component

--- web-sajtika/pages/index.js
+++ web-sajtika/pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import Contact from "../components/Contact";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
@@ -9,68 +9,7 @@ export default function Home() {
       <Head>
         <title>Programerika</title>
       </Head>
-      <nav className="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
-        {/* Avatar image in top left corner */}
-        <Image src="/img/logo.png" alt="me" width="200" height="200" />
-        <a href="#" className="w3-bar-item w3-button w3-padding-large w3-black">
-          <i className="fa fa-home w3-xxlarge" />
-          <p>HOME</p>
-        </a>
-        <a
-          href="#about"
-          className="w3-bar-item w3-button w3-padding-large w3-hover-black"
-        >
-          <i className="fa fa-user w3-xxlarge" />
-          <p>ABOUT</p>
-        </a>
-        <a
-          href="#photos"
-          className="w3-bar-item w3-button w3-padding-large w3-hover-black"
-        >
-          <i className="fa fa-eye w3-xxlarge" />
-          <p>PHOTOS</p>
-        </a>
-        <a
-          href="#contact"
-          className="w3-bar-item w3-button w3-padding-large w3-hover-black"
-        >
-          <i className="fa fa-envelope w3-xxlarge" />
-          <p>CONTACT</p>
-        </a>
-      </nav>
-      {/* Navbar on small screens (Hidden on medium and large screens) */}
-      <div className="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
-        <div className="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
-          <a
-            href="#"
-            className="w3-bar-item w3-button"
-            style={{ width: "25% !important" }}
-          >
-            HOME
-          </a>
-          <a
-            href="#about"
-            className="w3-bar-item w3-button"
-            style={{ width: "25% !important" }}
-          >
-            ABOUT
-          </a>
-          <a
-            href="#photos"
-            className="w3-bar-item w3-button"
-            style={{ width: "25% !important" }}
-          >
-            PHOTOS
-          </a>
-          <a
-            href="#contact"
-            className="w3-bar-item w3-button"
-            style={{ width: "25% !important" }}
-          >
-            CONTACT
-          </a>
-        </div>
-      </div>
+      <Navbar/>
       {/* Page Content */}
       <div className="w3-padding-large" id="main">
         {/* Header/Home */}
