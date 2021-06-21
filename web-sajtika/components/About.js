0f62b4458d924742f8234ad50f6af06ba320e714
Branko1303 <branko.saponjic@programerika.com>
2021-06-21 15:18:40 +0200

    Deleted Skils from About.js

--- web-sajtika/components/About.js
+++ web-sajtika/components/About.js
@@ -36,55 +36,6 @@ const About = () => {
           labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
           exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
-        <h3 className="w3-padding-16 w3-text-light-grey">My Skills</h3>
-        <p className="w3-wide">Photography</p>
-        <div className="w3-white">
-          <div
-            className="w3-dark-grey"
-            style={{ height: "28px", width: "95%" }}
-          />
-        </div>
-        <p className="w3-wide">Web Design</p>
-        <div className="w3-white">
-          <div
-            className="w3-dark-grey"
-            style={{ height: "28px", width: "85%" }}
-          />
-        </div>
-        <p className="w3-wide">Photoshop</p>
-        <div className="w3-white">
-          <div
-            className="w3-dark-grey"
-            style={{ height: "28px", width: "80%" }}
-          />
-        </div>
-        <br />
-        <div className="w3-row w3-center w3-padding-16 w3-section w3-light-grey">
-          <div className="w3-quarter w3-section">
-            <span className="w3-xlarge">11+</span>
-            <br />
-            Partners
-          </div>
-          <div className="w3-quarter w3-section">
-            <span className="w3-xlarge">55+</span>
-            <br />
-            Projects Done
-          </div>
-          <div className="w3-quarter w3-section">
-            <span className="w3-xlarge">89+</span>
-            <br />
-            Happy Clients
-          </div>
-          <div className="w3-quarter w3-section">
-            <span className="w3-xlarge">150+</span>
-            <br />
-            Meetings
-          </div>
-        </div>
-        <button className="w3-button w3-light-grey w3-padding-large w3-section">
-          <i className="fa fa-download" /> Download Resume
-        </button>
-
         {/* End About Section */}
       </div>
     </div>
