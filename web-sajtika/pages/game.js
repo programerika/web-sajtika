1089cf9c6cae5a65eb302658dd84137f5ce8bd0f
Milan Uzelac <milan.uzelac@programerika.com>
2021-06-29 11:31:27 +0200

    added test game npm package

--- web-sajtika/pages/game.js
+++ web-sajtika/pages/game.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { HelloComponent } from 'web-gejmika-frontend/dist/index'
 
 const game = () => {
   return (
-    <div className="w3-padding-large" id="main">
-      <h1>hello from game page</h1>
+    <div className="w3-padding-large w3-center" id="main">
+      <HelloComponent />
     </div>
   );
 };
