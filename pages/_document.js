798bad65b88bf76217b4c36f22bac226552c4ee7
Milan Uzelac <milan.uzelac@programerika.com>
2022-02-11 11:45:33 +0100

    added web-gejmika-front version 1.0.6 and new favicon icon

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="shortcut icon" href="../public/favicon.ico" />
+          <link rel="shortcut icon" href="../favicon.ico" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Montserrat"
