2ce46542fb31017bcffcc59ab919117f0d155373
sarakompirovic <sara.kompirovic@programerika.com>
2022-10-14 15:01:26 +0200

    Tabs for bombika and gejmika

---
+++ components/WebBombika.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import dynamic from "next/dynamic";
+
+const createBombika = (id) => {
+  const WebBombika = "";
+
+  useEffect(() => {
+    createBombika(id);
+  });
+};
+
+export default createBombika;
--- pages/game.js
+++ pages/game.js
@@ -1,5 +1,11 @@
-import React from "react";
+// import React from "react";
 import dynamic from "next/dynamic";
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import TabPanel from "@mui/material";
+// import Typography from "@mui/material/Typography";
+import createBombika from "../components/WebBombika";
 
 const WebGejmika = dynamic(
   () => {
@@ -9,21 +15,43 @@ const WebGejmika = dynamic(
 );
 
 const game = () => {
+  const [selectedTab, setSelectedTab] = React.useState("0");
+
+  const toggleTab = (event, newValue) => {
+    setSelectedTab(newValue);
+  };
+
+  createBombika("gameBombika");
+
   return (
-    <div className="w3-padding-large" id="main">
-      {/* Game Content */}
-      <div
-        className="w3-content w3-justify w3-text-grey w3-padding-16"
-        id="game"
-      >
-        <h2 className="w3-text-light-grey w3-center">Demo game</h2>
-        <hr className="w3-opacity w3-width-100" />
+    // <Box sx={{ width: "100%", bgcolor: "black" }}>
+    <>
+      <Tabs value={selectedTab} onChange={toggleTab} centered>
+        <Tab label="Gejmika" sx={{ color: "white" }} />
+        <Tab label="Bombika" sx={{ color: "white" }} />
+      </Tabs>
+      {selectedTab === 0 && (
         <div className="w3-padding-16">
           <WebGejmika />
         </div>
-      </div>
-      {/*End Game Content */}
-    </div>
+      )}
+      {selectedTab === 1 && <div id="gameBombika" />}
+    </>
+
+    // <div className="w3-padding-large" id="main">
+    //   {/* Game Content */}
+    //   <div
+    //     className="w3-content w3-justify w3-text-grey w3-padding-16"
+    //     id="game"
+    //   >
+    //     <h2 className="w3-text-light-grey w3-center">Demo game</h2>
+    //     <hr className="w3-opacity w3-width-100" />
+    //     <div className="w3-padding-16">
+    //       <WebGejmika />
+    //     </div>
+    //   </div>
+    //   {/*End Game Content */}
+    // </div>
   );
 };
 
