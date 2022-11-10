21a6c24fb841ece8fac02ac72ba9c06c80aedafa
Branko Saponjic <branko.saponjic@programerika.com>
2022-11-10 16:00:55 +0100

    refactor: @programerika/web-bombika npm package

--- components/WebBombika.js
+++ components/WebBombika.js
@@ -1,19 +1,9 @@
 import { useEffect } from "react";
 import "@mdi/font/css/materialdesignicons.css";
-//import dynamic from "next/dynamic";
-//import createBombika from "bombika";
-//import "bombika/dist/fonts/";
-// const createBombika = dynamic(
-//   () => {
-//     return import("bombika");
-//   },
-//   { ssr: false }
-// );
 
 export default () => {
   useEffect(async () => {
-    //console.log(createBombika.render());
-    const createBombikaModule = await import("bombika");
+    const createBombikaModule = await import("@programerika/web-bombika");
     createBombikaModule.default("#bombika");
   });
   return <div id="bombika"></div>;
