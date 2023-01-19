792d73e916b2cd2aad4f0a2731325788583e18c9
Vladimir Djekic <vladimir.djekic@programerika.com>
2023-01-19 21:50:24 +0100

    fix: added regenerator runtime import because of error "regeneratorRuntime is not defined"

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Layout from "../components/Layout";
 import "w3-css/w3.css";
 import "../styles/globals.css";
 import "@mdi/font/css/materialdesignicons.css";
+import "regenerator-runtime/runtime";
 
 function MyApp({ Component, pageProps }) {
   return (
